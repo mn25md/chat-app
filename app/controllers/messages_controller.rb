7f2f1c4a98baa1c46907293f7f047b36c9070730
mn25md <mn25md@gmail.com>
2024-07-08 15:29:47 +0900

    チャットルーム管理機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
-  def index
+
+ def index
     @message = Message.new
     @room = Room.find(params[:room_id])
     @messages = @room.messages.includes(:user)
@@ -17,7 +18,8 @@ class MessagesController < ApplicationController
   end
 
   private
+
   def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
