7ddaaf8925a356d5bc55354e32f33c24ed5a5a97
mn25md <mn25md@gmail.com>
2024-07-10 14:30:45 +0900

    総合テストの実装

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,41 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Message, type: :model do
+RSpec.describe 'メッセージ投稿機能', type: :system do
   before do
-    @message = FactoryBot.build(:message)
+    # 中間テーブルを作成して、usersテーブルとroomsテーブルのレコードを作成する
+    @room_user = FactoryBot.create(:room_user)
   end
 
-  describe 'メッセージ投稿' do
-    context 'メッセージが投稿できる場合' do
-      it 'contentとimageが存在していれば保存できる' do
-        expect(@message).to be_valid
-      end
-      it 'contentが空でも保存できる' do
-        @message.content = ''
-        expect(@message).to be_valid
-      end
-      it 'imageが空でも保存できる' do
-        @message.image = nil
-        expect(@message).to be_valid
-      end
-    end
-    context 'メッセージが投稿できない場合' do
-      it 'contentとimageが空では保存できない' do
-        @message.content = ''
-        @message.image = nil
-        @message.valid?
-        expect(@message.errors.full_messages).to include("Content can't be blank")
-      end
-      it 'roomが紐付いていないと保存できない' do
-        @message.room = nil
-        @message.valid?
-        expect(@message.errors.full_messages).to include('Room must exist')
-      end
-      it 'userが紐付いていないと保存できない' do
-        @message.user = nil
-        @message.valid?
-        expect(@message.errors.full_messages).to include('User must exist')
-      end
+  context '投稿に失敗したとき' do
+    it '送る値が空の為、メッセージの送信に失敗する' do
+      # サインインする
+      sign_in(@room_user.user)
+
+      # 作成されたチャットルームへ遷移する
+      click_on(@room_user.room.name)
+
+      # DBに保存されていないことを確認する
+      expect {
+        find('input[name="commit"]').click
+      }.not_to change { Message.count }
+
+      # 元のページに戻ってくることを確認する
+      expect(page).to have_current_path(room_messages_path(@room_user.room))
     end
   end
-end
